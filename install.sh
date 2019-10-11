4352e8540b461346b2df91f1912b1eea9e502a72
e-carlin <evan@carlin.com>
2019-10-10 22:07:13 -0600

    ctrlp

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# POST: no whitespace in filenames so ok
+for f in $(find dot bin nvim site -name '*' -type f); do 
+    echo "$f"
+
+    src=$PWD/$f
+    cmd='ln -s'
+    if [[ $src =~ bin/ ]]; then
+        chmod +x "$src"
+        dst="$HOME"/$f
+    elif [[ $src =~ nvim/ ]]; then
+        dst="$HOME"/.config/$f
+    elif [[ $src =~ site/ ]]; then
+        dst="$HOME"/.local/share/nvim/$f
+    else
+        dst="$HOME"/.${f#dot/}
+    fi
+    if [ -e "$dst" ]; then
+        if cmp --silent "$dst" "$src"; then
+            continue
+        fi
+        rm -f "$dst.old"
+	mv "$dst" "$dst.old"
+    fi
+    dir=$(dirname $dst)
+    if [[ ! -d $dir ]]; then
+#         echo "mkdir -p $dir"
+	mkdir -p "$dir"
+    fi
+    $cmd "$src" "$dst"
+#    echo "$cmd $src $dst"
+done
