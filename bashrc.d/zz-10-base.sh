822cdef185da79c462863453efc93f12051a2d2c
e-carlin <evan@carlin.com>
2019-10-20 16:55:48 -0600

    darwin settings

--- bashrc.d/zz-10-base.sh
+++ bashrc.d/zz-10-base.sh
@@ -16,16 +16,32 @@ linux() {
     fi
 }
 
-# darwin() {
-
-# }
-# unset -f darwin
+darwin() {
+    # Keyboard - enable hold down key to repeat
+    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+    # Keyboard - set a blazingly fast key repeat rate
+    defaults write NSGlobalDomain KeyRepeat -int 1
+    defaults write NSGlobalDomain InitialKeyRepeat -int 10
+    # Finder - how all filename extensions
+    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+    # Finder - show hidden files (not so hidden after all)
+    defaults write com.apple.finder AppleShowAllFiles YES
+    # Finder - show path bar
+    defaults write com.apple.finder ShowPathbar -bool true
+    # Finder - when performing a search, search the current folder by default
+    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    # OS - disable resume system-wide
+    defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+}
 
 readonly OS="$(uname -a | cut -d ' ' -f1)"
 if [[ "${OS}" = "Linux" ]]; then
     linux
+elif [[ "${OS}" = "Darwin" ]]; then
+    darwin
 fi
 unset -f linux
+unset -f darwin
 
 
 export EDITOR=$(type -p nvim)
