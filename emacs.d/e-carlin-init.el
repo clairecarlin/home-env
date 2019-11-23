9fa08e615930584c9af302c04dad2e4c70b43373
Evan Carlin <evan@carlin.com>
2019-11-23 16:32:52 +0000

    move yasnippets under src control

--- emacs.d/e-carlin-init.el
+++ emacs.d/e-carlin-init.el
@@ -186,4 +186,5 @@
 (add-to-list 'load-path
               "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
+(setq yas-snippet-dirs '("~/src/e-carlin/home-env/emacs.d/e-carlin-yasnippets")) ; must be above yas-global-mode
 (yas-global-mode 1)
