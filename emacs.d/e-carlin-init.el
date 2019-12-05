847c1f96c0c94e27611ee2f87e8666c71b032835
Evan Carlin <evan@carlin.com>
2019-12-05 04:34:18 +0000

    xterm-color for coloring in shell

--- emacs.d/e-carlin-init.el
+++ emacs.d/e-carlin-init.el
@@ -54,8 +54,19 @@
 (setq kill-buffer-query-functions nil)
 
 ;; Allow the evil escape characters to do their thing
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-(add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
+;; (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+;; (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
+(setq comint-output-filter-functions
+      (remove 'ansi-color-process-output comint-output-filter-functions))
+
+(add-hook 'shell-mode-hook
+          (lambda ()
+            ;; Disable font-locking in this buffer to improve performance
+            (font-lock-mode -1)
+            ;; Prevent font-locking from being re-enabled in this buffer
+            (make-local-variable 'font-lock-function)
+            (setq font-lock-function (lambda (_) nil))
+            (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)))
 
 
 ;; C-f1 shows currently editing filename
