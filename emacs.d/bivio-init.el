b96fb1eaccefeec3826c3655614c40644807e46a
Evan Carlin <evan@carlin.com>
2019-12-12 04:07:57 +0000

    random changes; should make lsp mode better

--- emacs.d/bivio-init.el
+++ emacs.d/bivio-init.el
@@ -15,7 +15,7 @@
 (require 'mmm-mode)
 (require 'uniquify)
 ;;; remove after emacs 23 is gone
-(ignore-errors (require 'web-mode))
+;; (ignore-errors (require 'web-mode))
 (ignore-errors (require 'espresso))
 (ignore-errors (require 'java-mode))
 
@@ -98,22 +98,22 @@
 
 (define-key java-mode-map "\C-c\C-m" 'compile)
 
-(if (fboundp 'web-mode)
-    (progn
-      (setq web-mode-code-indent-offset 4)
-      (setq web-mode-css-indent-offset 4)
-      (setq web-mode-markup-indent-offset 4)
-      (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . web-mode))
-      (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))))
+;; (if (fboundp 'web-mode)
+;;     (progn
+;;       (setq web-mode-code-indent-offset 4)
+;;       (setq web-mode-css-indent-offset 4)
+;;       (setq web-mode-markup-indent-offset 4)
+;;       (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . web-mode))
+;;       (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))))
 
 
 (add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
