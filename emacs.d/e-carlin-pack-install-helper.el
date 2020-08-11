3b144ec063ff873a6540bbe519b132f898528d1f
Evan Carlin <evan@carlin.com>
2020-08-11 09:36:55 -0600

    better install instructions

---
+++ emacs.d/e-carlin-pack-install-helper.el
@@ -0,0 +1,4 @@
+ (setq package-selected-packages
+   (quote
+    (flymake-json srefactor slime lsp-ui arduino-mode xterm-color pyenv-mode xclip find-file-in-repository evil-commentary company-lsp helm-lsp lsp-python-ms lsp-mode company)))
+(package-install-selected-packages)
