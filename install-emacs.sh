1e8587b2f35da4748f773b4b49223332dfbc57bf
Evan Carlin <evan@carlin.com>
2020-08-27 10:20:16 -0600

    add install emacs script

---
+++ install-emacs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function main() {
+    if emacs --version; then
+        echo "Existing emacs found please uninstall"
+        exit 1
+    fi
+
+    if ! pkg-config --exists --print-errors "jansson >= 2.7"; then
+        echo "jansson not found. Need to install (ex yum install jansson-devel)"
+        exit 1
+    fi
+
+    cd /tmp
+    mkdir e
+    cd e
+    wget https://ftp.gnu.org/pub/gnu/emacs/emacs-27.1.tar.gz
+    tar -zxf emacs-27.1
+    cd emacs-27.1
+    ./configure --with-x-toolkit=no --without-x
+    make
+    echo 'cd /tmp/e && sudo make install'
+}
+
+main
