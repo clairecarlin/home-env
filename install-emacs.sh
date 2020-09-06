e467c82d486794e5156ba7ef2fd061ebcd2847a6
e-carlin <evan@carlin.com>
2020-09-06 15:09:26 -0600

    emacs on mac; make idempotent

--- install-emacs.sh
+++ install-emacs.sh
@@ -3,25 +3,30 @@
 set -euo pipefail
 
 function main() {
-    if emacs --version; then
+    if emacs --version > /dev/null 2>&1; then
         echo "Existing emacs found please uninstall"
         exit 1
     fi
 
-    if ! pkg-config --exists --print-errors "jansson >= 2.7"; then
+    if ! pkg-config --version > /dev/null 2>&1; then
+       echo "pkg-config not installed"
+       exit 1
+    fi
+
+    if ! pkg-config --exists "jansson >= 2.7" > /dev/null 2>&1; then
         echo "jansson not found. Need to install (ex yum install jansson-devel)"
         exit 1
     fi
 
-    cd /tmp
-    mkdir e
-    cd e
-    wget https://ftp.gnu.org/pub/gnu/emacs/emacs-27.1.tar.gz
-    tar -zxf emacs-27.1.tar.gz
+    cd ~
+    if [[ ! -f emacs-27.1.tar.gz || ! -d emacs-27.1 ]]; then
+       wget https://ftp.gnu.org/pub/gnu/emacs/emacs-27.1.tar.gz
+       tar -zxf emacs-27.1.tar.gz
+    fi
     cd emacs-27.1
     ./configure --with-x-toolkit=no --without-x
     make
-    echo 'cd /tmp/e && sudo make install'
+    echo 'cd ~/emacs-27.1 && sudo make install'
 }
 
 main
