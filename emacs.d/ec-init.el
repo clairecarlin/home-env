3ef5ccf570c82a10fad3865837199542c110c39a
e-carlin <evan@carlin.com>
2021-06-02 20:03:43 +0000

    remove pyenv for now

--- emacs.d/ec-init.el
+++ emacs.d/ec-init.el
@@ -23,6 +23,7 @@
     (arduino-mode company evil evil-commentary find-file-in-repository flymake flycheck helm lsp-mode helm-lsp lsp-mode lsp-python-ms lsp-ui markdown-mode pyenv-mode slime srefactor undo-tree xterm-color)))
 
 (require 'ec-radiasoft)
+(require 'ec-util)
 (require 'srefactor)
 (require 'srefactor-lisp)
 
