7d02b52c61c51edf7c181292ddb148603fa3eabf
Claire Carlin <claire@carlin.com>
2023-12-25 17:00:01 -0700

    Setup conda

--- bashrc.d/zz-10-base.sh
+++ bashrc.d/zz-10-base.sh
@@ -3,3 +3,19 @@ export HISTSIZE=$s
 export HISTFILESIZE=$s
 export PATH="$HOME/bin:$PATH"
 export PS1="\W$ "
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/carlin/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/carlin/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/carlin/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/carlin/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
